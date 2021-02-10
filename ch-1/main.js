78968a9b0c3f635fcd28f7408604440c4c5d5658
DarkShadow20 <kunal8011@gmail.com>
2021-02-10 11:08:19 +0530

    Soultions

---
+++ ch-1/main.js
@@ -0,0 +1,2 @@
+var a ="a newline character is written like \"\\n\".";
+console.log(a)
---
+++ ch-2/ex3/main.js
@@ -0,0 +1,12 @@
+board="";
+for(var i=0;i<8;i++){
+    for(var j=0;j<8;j++){
+        if((i+j)%2 == 0){
+            board +=" ";
+        }
+        else
+            board +="#";
+    }
+    board +="\n";
+}
+console.log(board)
